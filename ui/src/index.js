4a2af565bbdcc377e9171d2374632a412dfb27b5
Alex Bea <abea@mailbox.org>
2021-11-02 14:15:44 -0500

    wip

---
+++ ui/src/index.js
@@ -0,0 +1,10 @@
+export default () => {
+  // Allow Apostrophe to create the bus first
+  setTimeout(function() {
+    apos.bus && apos.bus.$on('export-download', (event) => {
+      if (event.url) {
+        window.open(event.url, '_blank');
+      }
+    });
+  }, 0);
+};
