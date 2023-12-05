e2126e5371343d217785f1f0e781a3159060ca67
Etienne Laurent <etldev@tuta.io>
2023-12-05 14:54:32 +0100

    add csv to label

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ module.exports = {
     return {
       add: {
         export: {
-          label: 'Export',
+          label: 'Export as CSV',
           route: '/export',
           messages: {
             progress: 'Exporting {{ type }}...'
